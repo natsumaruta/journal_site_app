3604956a267e0267a4238fafdef8ddaea8830e45
yonezawanatsuko <nabeta0915@gmail.com>
2025-12-07 22:14:46 +0900

    ルーティングとblade.php作成

---
+++ resources/views/livewire/articles/index.blade.php
@@ -0,0 +1,21 @@
+<?php
+
+use function Livewire\Volt\{state};
+use App\Models\Article;
+
+state(['articles' => fn() => Article::all()]);
+
+?>
+
+<div>
+    <h1>論文一覧</h1>
+    <ul>
+        @foreach ($articles as $article)
+            <li>
+                <a href="{{ route('articles.show', $article) }}">
+                    {{ $article->title }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
---
+++ resources/views/livewire/articles/show.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+use function Livewire\Volt\{state};
+use App\Models\Article;
+
+// ルートモデルバインディング
+state(['article' => fn(Article $article) => $article]);
+
+?>
+
+<div>
+    <a href="{{ route('articles.index') }}">戻る</a>
+    <h1>{{ $article->title }}</h1>
+    <p>{!! nl2br(e($article->body)) !!}</p>
+</div>
