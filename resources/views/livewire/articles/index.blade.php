666e96749feeef10ab052d99758e7990319b2c40
yonezawanatsuko <nabeta0915@gmail.com>
2025-12-07 22:26:15 +0900

    課題2完了

--- resources/views/livewire/articles/index.blade.php
+++ resources/views/livewire/articles/index.blade.php
@@ -5,6 +5,10 @@ use App\Models\Article;
 
 state(['articles' => fn() => Article::all()]);
 
+$create = function () {
+    return redirect()->route('articles.create');
+};
+
 ?>
 
 <div>
@@ -18,4 +22,5 @@ state(['articles' => fn() => Article::all()]);
             </li>
         @endforeach
     </ul>
+    <button wire:click="create">新規論文投稿</button>
 </div>
--- resources/views/livewire/articles/show.blade.php
+++ resources/views/livewire/articles/show.blade.php
@@ -6,10 +6,25 @@ use App\Models\Article;
 // ルートモデルバインディング
 state(['article' => fn(Article $article) => $article]);
 
+$back = function () {
+    return redirect()->route('articles.index');
+};
+
+// $edit = function () {
+//     return redirect()->route('articles.index');
+// };
+
+// $delete = function () {
+//     return redirect()->route('articles.index');
+// };
+
 ?>
 
 <div>
     <a href="{{ route('articles.index') }}">戻る</a>
     <h1>{{ $article->title }}</h1>
     <p>{!! nl2br(e($article->body)) !!}</p>
+    <button wire:click="back">一覧へ戻る</button>
+    {{-- <button wire:click="create">編集する</button>
+    <button wire:click="create">削除する</button> --}}
 </div>
